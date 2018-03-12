1ad03d3991fd491c63a077b5590452bd661998d3
zooozoo <shz0309@gmail.com>
2018-03-12 15:02:12 +0900

    기능 테스트가 작업 아이템을 추가할 수 있는지 확인

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 import unittest
 
+from selenium.webdriver.common.keys import Keys
+
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -16,16 +19,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다
+        # run server를 실행 시켜야함(그렇지 않으면 'localhost'로 나온다.)
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 그녀는 바로 작업을 추가하기로 한다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
 
         # "공작깃털 사기"라고 텍스트 상자에 입력한다:
         # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
+        inputbox.send_keys('공작깃털 사기')
 
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에
         # "1: 공작깃털 사기" 아이템이 추가된다
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다(에디스틑 매우 체계적인 사람이다)
@@ -38,6 +56,9 @@ class NewVisitorTest(unittest.TestCase):
         # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
 
         # 만족하고 잠자리에 든다
+
+        self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
