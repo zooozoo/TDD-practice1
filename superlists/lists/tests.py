ef2d93bad7e041197ae172df3ab57cc1c7a5bfa8
zooozoo <shz0309@gmail.com>
2018-03-12 14:06:19 +0900

    최소 html을 반환하는 기본 뷰

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -12,6 +12,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.statswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists<title>', response.content)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
