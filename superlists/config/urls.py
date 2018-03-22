ff9df863cb80fc7db374c94b0689c7725bdde5e6
zooozoo <shz0309@gmail.com>
2018-03-22 16:13:49 +0900

    첫 단위 테스트와 url 맵핑 그리고 임시 뷰

--- superlists/config/urls.py
+++ superlists/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+import lists.views
 
 urlpatterns = [
+    url(r'^$', lists.views.home_page, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+from django.urls import resolve
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+    def test_root_rul_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
