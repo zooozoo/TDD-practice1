7abff7f8e71fa15c08cf3fb7091934b3318bf42e
zooozoo <shz0309@gmail.com>
2018-03-22 14:32:20 +0900

    tdd 2dn first commit

--- superlists/config/urls.py
+++ superlists/config/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from lists import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/', include('lists.urls')),
     url(r'^admin/', admin.site.urls),
 ]
