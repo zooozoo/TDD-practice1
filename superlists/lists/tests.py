f3f3021c4caee1cbcfbdf514294cf289efabcc07
zooozoo <shz0309@gmail.com>
2018-03-27 20:48:48 +0900

    POST 후에 리디렉션과 템플릿에 보든 작업 아이템 표시

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,6 +8,7 @@ from lists.views import home_page
 
 from lists.models import Item
 
+
 class HomePageTest(TestCase):
     def test_root_rul_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -18,9 +19,22 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_caan_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': '신규 작업 아이템'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+    def test_home_page_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -39,3 +53,12 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
