e47659296665e8a8a172afdebc1e0d0b39a1c83b
zooozoo <shz0309@gmail.com>
2018-03-30 14:49:32 +0900

    2회독 마무리

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -23,4 +23,4 @@ def new_list(request):
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id),)
+    return redirect('/lists/%d/' % (list_.id), )
