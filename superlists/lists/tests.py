17021995b7e34a1ce512d340704b9634199189f2
zooozoo <shz0309@gmail.com>
2018-03-30 17:06:52 +0900

    아이템용 모델과 마이그레이션 연계

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -7,6 +7,7 @@ from django.test import TestCase
 # Create your tests here.
 from django.urls import resolve
 from lists.views import home_page
+from .models import Item
 
 
 class HomePageTest(TestCase):
@@ -41,3 +42,21 @@ class HomePageTest(TestCase):
             re.sub(self.pattern_input_csrf, '', response.content.decode()),
             re.sub(self.pattern_input_csrf, '', expected_html)
         )
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '첫 번째 아이템'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '두 번째 아이템'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(second_saved_item.text, '두 번째 아이템')
