6ed54ef462e5d35687b8ff0a8153dfa4e885c3d8
zooozoo <shz0309@gmail.com>
2018-03-30 20:40:10 +0900

    functioal_tests 앱 생성 및 LiveServerTestCase 사용

--- superlists/ft/tests.py
+++ superlists/ft/tests.py
@@ -1,10 +1,11 @@
 import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith)는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다
         self.assertIn('To-Do', self.browser.title)
@@ -62,9 +63,3 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')
 
         # 만족하고 잠자리에 든다
-
-
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
